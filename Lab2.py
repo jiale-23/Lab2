6e66f8e8b89e1a34a05e213164af098ffe4977c9
jiale <jiale.23@ichat.sp.edu.sg>
2025-11-30 15:33:24 +0800

    3rd commit

--- Lab2.py
+++ Lab2.py
@@ -1,1 +1,11 @@
-print("Hello World!" + "3")
+def cal_bmi(h, w):
+    x = w/h**2
+    if (x < 18.5):
+        return "Underweight"
+    elif (x <= 25):
+        return "Normal"
+    else:
+        return "Overweight"
+
+
+print(cal_bmi(h=1.74, w=76.4))
